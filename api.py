72be864e467a1a62b704be976e9a0bf51cdc448d
tavi <tavi2105@gmail.com>
2023-02-26 23:42:51 +0200

    check

--- api.py
+++ api.py
@@ -1,22 +1,31 @@
+import flask
 from flask import Flask, request, json
-
+from flask_cors import CORS
 from logic import create_list, add_annotation
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/contexts', methods=['GET'])
 def get():
     args = request.args
     data = create_list(args["word"])
-    return data, 200
+
+    response = flask.jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/annotation', methods=['POST'])
 def post():
     data = json.loads(request.data)
+    print(data)
     add_annotation(data["id"], data["context"], data["context_id"])
-    return {"Status": "Success"}, 200
+
+    response = flask.jsonify({"Status": "Success"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
